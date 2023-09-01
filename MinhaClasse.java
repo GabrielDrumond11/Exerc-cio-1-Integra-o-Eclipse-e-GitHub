3bc29c0dea3b2b0d7aa200516a44b2bebabbe2fd
GabrielDrumond11 <143820393+GabrielDrumond11@users.noreply.github.com>
2023-09-01 11:58:58 -0300

    Add files via upload

---
+++ MinhaClasse.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+
+	public class MinhaClasse {
+	    public static void main(String[] args) {
+	        int numero1 = 5;
+	        int numero2 = 7;
+	        int soma = numero1 + numero2;
+	        System.out.println("A soma de " + numero1 + " e " + numero2 + " é igual a " + soma);
+	    }
+	}
